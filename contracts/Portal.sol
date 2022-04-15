e744a8659970ee65810d6adb024179573198bc11
Sharad Chand <pepsighan@sharadchand.com>
2022-04-15 23:33:53 +0530

    Extract the majority part of the code into Portal abstract contract

---
+++ contracts/Portal.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+/// Portal is wormhole which receives tokens on one end in a network and ejects tokens on the
+/// other end on a different network.
+abstract contract Portal is Ownable {
+    using Counters for Counters.Counter;
+
+    /// The direction in which a token is transferred to or out the vault.
+    enum TransferDirection {
+        IN,
+        OUT
+    }
+
+    /// The counter for transfer id.
+    Counters.Counter internal _counter;
+
+    /// The address of the TNT20 token that is transferred against a transfer id.
+    mapping(uint256 => address) public tokenAddress;
+
+    /// The address which transferred the amount against a transfer id.
+    mapping(uint256 => address) public transferredBy;
+
+    /// The address which is receiving the amount against a transfer id.
+    mapping(uint256 => address) public transferredTo;
+
+    /// The direction of transfer against a transfer id.
+    mapping(uint256 => TransferDirection) public transferDirection;
+
+    /// The amount that is received against a transfer id.
+    mapping(uint256 => uint256) public transferredAmount;
+
+    /// The uint256 id used in the above mapping. There are two ways in which ids are used. It can
+    /// either be generated within this portal or is generated on another portal. A portal always
+    /// has a unique linked portal on the other end. To separate the id generated in this portal from
+    /// ids generated on another, this partition is used.
+    /// The result: the id value is uint255 in reality.
+    uint256 private immutable _parition = 1 << 255;
+
+    /// Get the partitioned id of an external id.
+    function _partitionedId(uint256 id) internal pure returns (uint256) {
+        return id + _parition;
+    }
+
+    /// Store the record of transfer.
+    function store(
+        uint256 id,
+        address token,
+        address from,
+        address to,
+        TransferDirection direction,
+        uint256 amount
+    ) internal {
+        require(
+            tokenAddress[id] == address(0) &&
+                transferredBy[id] == address(0) &&
+                transferredTo[id] == address(0) &&
+                transferredAmount[id] == 0,
+            'Portal: DUPLICATE_TRANSFER'
+        );
+
+        tokenAddress[id] = token;
+        transferredBy[id] = from;
+        transferredTo[id] = to;
+        transferDirection[id] = direction;
+        transferredAmount[id] = amount;
+    }
+
+    /// Mints the token of a given type and sends it. Only run this once respective token
+    /// is locked on the Root/ETH vault.
+    function send(
+        address token,
+        uint256 _id,
+        address from,
+        address to,
+        uint256 amount
+    ) public virtual;
+
+    /// Burns the amount of tokens sent to this vault and stores the address which is
+    /// going to receive the respective tokens on the root chain.
+    function withdraw(
+        address token,
+        address to,
+        uint256 amount
+    ) public virtual returns (uint256);
+}

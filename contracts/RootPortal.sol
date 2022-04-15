53ffb7dcf8dc19de66a0db2a5743feecb919043f
Sharad Chand <pepsighan@sharadchand.com>
2022-04-15 23:50:43 +0530

    Support sending ETH

--- contracts/RootPortal.sol
+++ contracts/RootPortal.sol
@@ -11,7 +11,7 @@ contract RootPortal is Portal {
     using Counters for Counters.Counter;
     using SafeERC20 for IERC20;
 
-    /// Deposits the amount of tokens to this vault and stores the address which is
+    /// Deposits the amount of tokens to this portal and stores the address which is
     /// going to receive the respective tokens on the other chain.
     function send(
         address token,
@@ -25,6 +25,20 @@ contract RootPortal is Portal {
 
         IERC20 erc20 = IERC20(token);
         erc20.safeTransferFrom(msg.sender, address(this), amount);
+        return id;
+    }
+
+    /// Deposits ETH to this portal and stores the address which is going to receive the
+    /// respective token on the other chain.
+    function sendETH(address to, uint256 amount) public payable returns (uint256) {
+        // Register the details of the transfer.
+        uint256 id = _counter.current();
+        _counter.increment();
+
+        // Because ETH is not a ERC20 token, we need another unique address to record this transfer.
+        // Address of this contract itself works.
+        address token = address(this);
+        store(id, token, msg.sender, to, TransferDirection.OUT, amount);
         return id;
     }
 }

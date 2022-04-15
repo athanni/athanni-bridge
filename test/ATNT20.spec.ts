ec27c984223314d3649a7c94dffced5ea1bfa15f
Sharad Chand <pepsighan@sharadchand.com>
2022-04-15 14:19:15 +0530

    Add a dummy test

---
+++ test/ATNT20.spec.ts
@@ -0,0 +1,9 @@
+import chai from 'chai';
+import { solidity } from 'ethereum-waffle';
+
+chai.use(solidity);
+
+// TODO: Write down the tests.
+describe('ATNT20', () => {
+  it('mint', async () => {});
+});

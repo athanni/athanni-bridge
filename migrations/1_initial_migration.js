14b3f2db23dfa9d064cd59ee971a07da994b62e7
Sharad Chand <pepsighan@sharadchand.com>
2022-04-16 14:07:28 +0530

    Based on the network, deploy contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,13 @@
 const RootPortal = artifacts.require('./RootPortal');
 const ChildPortal = artifacts.require('./ChildPortal');
 
+/// Deploy the portals to the respective networks.
 module.exports = async function (deployer) {
-  await deployer.deploy(RootPortal);
-  await deployer.deploy(ChildPortal);
+  switch (deployer.network) {
+    case 'thetaNetwork':
+      await deployer.deploy(ChildPortal);
+      return;
+    case 'rinkeby':
+      await deployer.deploy(RootPortal);
+  }
 };

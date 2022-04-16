a942213cb256d3ca681bb6e95a74350f34c0a2cb
Sharad Chand <pepsighan@sharadchand.com>
2022-04-16 13:22:02 +0530

    Update the solc version

--- truffle-config.js
+++ truffle-config.js
@@ -1,36 +1,36 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
 module.exports = {
   contracts_build_directory: './build',
   networks: {
     thetaTestnet: {
       provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, 'https://eth-rpc-api-testnet.thetatoken.org/rpc'),
-      network_id: 365
+      network_id: 365,
     },
     thetaMainnet: {
       provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, 'https://eth-rpc-api.thetatoken.org/rpc'),
-      network_id: 361
+      network_id: 361,
     },
     maticTestnet: {
       provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, 'https://matic-mumbai.chainstacklabs.com'),
-      network_id: 80001
-    }
+      network_id: 80001,
+    },
   },
   compilers: {
     solc: {
-      version: '0.6.6',
+      version: '0.8.13',
       settings: {
         optimizer: {
           enabled: true,
           // This will generate gas efficient code for as many executions.
-          runs: 999999
-        }
-      }
-    }
+          runs: 999999,
+        },
+      },
+    },
   },
   plugins: ['truffle-plugin-verify'],
   api_keys: {
-    polygonscan: process.env.POLYGONSCAN_KEY
-  }
-}
+    polygonscan: process.env.POLYGONSCAN_KEY,
+  },
+};
